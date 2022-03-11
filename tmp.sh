0784662f56b088c425db118c2cc04ea291d23993
Yao Hwang <yaohwang99@gmail.com>
2022-03-12 00:21:18 +0800

    Add script for isolating one core
    Isolate cpu core number 7 for stable performance,
    restore after process is executed.

---
+++ tmp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#This script tweak the system setting to minimize the unstable factors while
+#analyzing the performance of fibdrv.
+#
+#2020/3/21  ver 1.0
+CPUID=7
+ORIG_ASLR=`cat /proc/sys/kernel/randomize_va_space`
+ORIG_GOV=`cat /sys/devices/system/cpu/cpu$CPUID/cpufreq/scaling_governor`
+ORIG_TURBO=`cat /sys/devices/system/cpu/intel_pstate/no_turbo`
+
+sudo bash -c "echo 0 > /proc/sys/kernel/randomize_va_space"
+sudo bash -c "echo performance > /sys/devices/system/cpu/cpu$CPUID/cpufreq/scaling_governor"
+sudo bash -c "echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo"
+
+
+sudo taskset -c 7 ./client_stat
+
+# restore the original system settings
+sudo bash -c "echo $ORIG_ASLR >  /proc/sys/kernel/randomize_va_space"
+sudo bash -c "echo $ORIG_GOV > /sys/devices/system/cpu/cpu$CPUID/cpufreq/scaling_governor"
+sudo bash -c "echo $ORIG_TURBO > /sys/devices/system/cpu/intel_pstate/no_turbo"
