e2dc77dee1a0a2f3bbadebc380960642bd45ce0e
Yao Hwang <yaohwang99@gmail.com>
2022-03-12 13:46:59 +0800

    Add multiple sampling

--- client_stat.c
+++ client_stat.c
@@ -6,10 +6,12 @@
 #include <time.h>
 #include <unistd.h>
 #define FIB_DEV "/dev/fibonacci"
-
+#define SAMPLE_TIME 10
+#define TIME_BUF_SIZE 2048
 int main()
 {
     char buf[1];
+
     char write_buf[] = "testing writing";
     int offset = 100; /* TODO: try test something bigger than the limit */
     FILE *fp = fopen("scripts/stat.txt", "w");
@@ -19,18 +21,22 @@ int main()
         perror("Failed to open character device");
         exit(1);
     }
-
     for (int i = 0; i <= offset; i++) {
-        long long sz, sz2;
-        lseek(fd, i, SEEK_SET);
-        clock_gettime(CLOCK_MONOTONIC, &t1);
-        sz = read(fd, buf, 1);
-        clock_gettime(CLOCK_MONOTONIC, &t2);
-        sz2 = write(fd, write_buf, 0);
-        fprintf(fp, "%d %ld %lld\n", i, t2.tv_nsec - t1.tv_nsec, sz2);
+        char time_buf[TIME_BUF_SIZE];
+        int used = 0;
+        for (int j = 0; j < SAMPLE_TIME; j++) {
+            long long sz, sz2;
+            lseek(fd, i, SEEK_SET);
+            clock_gettime(CLOCK_MONOTONIC, &t1);
+            sz = read(fd, buf, 1);
+            clock_gettime(CLOCK_MONOTONIC, &t2);
+            sz2 = write(fd, write_buf, 0);
+            snprintf(&time_buf[used], TIME_BUF_SIZE - used, "%ld %lld ",
+                     (long int) (t2.tv_nsec - t1.tv_nsec), sz2);
+            used = strnlen(time_buf, TIME_BUF_SIZE);
+        }
+        fprintf(fp, "%d %s\n", i, time_buf);
     }
-
-
     close(fd);
     fclose(fp);
     return 0;
