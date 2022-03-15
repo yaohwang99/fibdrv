4d304c2008e1067c6168befd70f862914fd66189
Yao Hwang <yaohwang99@gmail.com>
2022-03-16 00:55:06 +0800

    Output median of given sampling time

--- client_stat.c
+++ client_stat.c
@@ -6,38 +6,61 @@
 #include <time.h>
 #include <unistd.h>
 #define FIB_DEV "/dev/fibonacci"
-#define SAMPLE_TIME 10
-#define TIME_BUF_SIZE 2048
+#define SAMPLE_TIME 21
+#define OFFSET 100
+int cmpfunc(const void *a, const void *b)
+{
+    return (*(int *) a - *(int *) b);
+}
 int main()
 {
-    char buf[1];
-
+    char big_num_buf[100];
     char write_buf[] = "testing writing";
-    int offset = 100; /* TODO: try test something bigger than the limit */
-    FILE *fp = fopen("scripts/stat.txt", "w");
+    // int OFFSET = 100; /* TODO: try test something bigger than the limit */
+    int **sample_kernel = malloc((OFFSET + 1) * sizeof(int *));
+    int **sample_user = malloc((OFFSET + 1) * sizeof(int *));
+    FILE *fpm = fopen("scripts/stat_med.txt", "w");
     struct timespec t1, t2;
     int fd = open(FIB_DEV, O_RDWR);
     if (fd < 0) {
         perror("Failed to open character device");
         exit(1);
     }
-    for (int i = 0; i <= offset; i++) {
-        char time_buf[TIME_BUF_SIZE];
-        int used = 0;
-        for (int j = 0; j < SAMPLE_TIME; j++) {
-            long long sz, sz2;
+    for (int i = 0; i <= OFFSET; i++) {
+        sample_user[i] = malloc(SAMPLE_TIME * sizeof(int));
+        sample_kernel[i] = malloc(SAMPLE_TIME * sizeof(int));
+    }
+    for (int j = 0; j < SAMPLE_TIME; j++) {
+        for (int i = 0; i <= OFFSET; i++) {
+            int used = 0;
             lseek(fd, i, SEEK_SET);
+            long long sz, sz2;
             clock_gettime(CLOCK_MONOTONIC, &t1);
-            sz = read(fd, buf, 1);
+            sz = read(fd, big_num_buf, 2);
             clock_gettime(CLOCK_MONOTONIC, &t2);
-            sz2 = write(fd, write_buf, 0);
-            snprintf(&time_buf[used], TIME_BUF_SIZE - used, "%ld %lld ",
-                     (long int) (t2.tv_nsec - t1.tv_nsec), sz2);
-            used = strnlen(time_buf, TIME_BUF_SIZE);
+            sz2 = write(fd, write_buf, 2);
+            sample_kernel[i][j] = (int) sz2;
+            sample_user[i][j] = (int) (t2.tv_nsec - t1.tv_nsec);
         }
-        fprintf(fp, "%d %s\n", i, time_buf);
+    }
+    for (int i = 0; i <= OFFSET; i++) {
+        lseek(fd, i, SEEK_SET);
+        long long sz3 = read(fd, big_num_buf, 2);
+        printf("f_big_num(%d): %s\n", i, big_num_buf);
+        if (sz3)
+            printf("f_big_num(%d) is truncated\n", i);
+        qsort(sample_kernel[i], SAMPLE_TIME, sizeof(int), cmpfunc);
+        qsort(sample_user[i], SAMPLE_TIME, sizeof(int), cmpfunc);
+        fprintf(fpm, "%d %d %d\n", i, sample_user[i][SAMPLE_TIME / 2],
+                sample_kernel[i][SAMPLE_TIME / 2]);
     }
     close(fd);
-    fclose(fp);
+    fclose(fpm);
+    for (int i = 0; i <= OFFSET; i++) {
+        free(sample_user[i]);
+        free(sample_kernel[i]);
+    }
+    free(sample_kernel);
+    free(sample_user);
     return 0;
 }
